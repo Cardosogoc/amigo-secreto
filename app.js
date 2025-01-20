4a57b8d42874833d5f3cf5512e1c9014c4cd8381
Gabriel de Oliveira Cardoso <108769102+Cardosogoc@users.noreply.github.com>
2025-01-20 08:56:01 -0300

    Adcionando a lista

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function textoNaTela (marcador, texto) {
+    let campo = document.querySelector(marcador);
+    campo.innerHTML = texto;
+}
+
+function mensagemInicial(){
+    textoNaTela("h2", "Digite o nome dos seus amigos");
+    textoNaTela("h1", "Amigo Secreto");
+}
+
+mensagemInicial();
+
+function adicionarAmigo () {
+    let nomeAmigo = document.querySelector("input").value;
+    if (nomeAmigo != ""){
+        if (listaDeAmigos.includes(nomeAmigo)){
+            alert("Este Amigo já está na lista!");
+            limparCampo();
+        } else {
+            listaDeAmigos.push(nomeAmigo);
+            limparCampo();  
+            listaNaTela();
+            console.log(listaDeAmigos);
+        }
+    } else {
+        alert("Insira um valor para que possa ser adcionado!");
+    }
+}
+
+function limparCampo() {
+    nomeAmigo = document.querySelector("input");
+    nomeAmigo.value = "";
+}
+
+function listaNaTela () {
+    let itemLista = document.getElementById("listaAmigos");
+    itemLista.innerHTML = "";
+    let qtdLista = listaDeAmigos.length;
+    for (let conta = 0; conta < qtdLista; conta++ ){
+        itemLista.innerHTML += `<li>${listaDeAmigos[conta]}</li>`
+    }
+}
+
